c99fe1622436045c60345c7f2fecd42fc46f3db3
David Strauch <dmaster18@gmail.com>
2020-12-25 18:33:11 +0000

    Automatically backed up by Learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,8 +1,20 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
+// ./src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'; /* code change */
+import { Provider } from 'react-redux';
+import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
+import App from './App';
+import './index.css';
+
+const store = createStore(shoppingListItemReducer, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_());
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
