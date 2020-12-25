2eb5f98650ad9662e2ca0560ab703317abc34f73
David Strauch <dmaster18@gmail.com>
2020-12-25 18:33:33 +0000

    Automatically backed up by Learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,28 +1,20 @@
-// ./src/index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
-import { Provider } from 'react-redux';
-import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
-
-const store = createStore(shoppingListItemReducer, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_());
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'));
-
+export default function shoppingListItemReducer(
+	state = {
+		items: []
+	},
+	action
 ) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length)
+			console.log('Updating state.items.length to %s', state.items.length + 1);
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log('Initial state.items length: %s', state.items.length);
 			return state;
 	}
 }
